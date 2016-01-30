c34ac44ea39af959b47cc1c8e0bc9c7147ad0563
saulocamposi <saulocamposi@gmail.com>
2016-01-30 21:12:39 -0200

    Create collection of json

--- JS/pushjson.js
+++ JS/pushjson.js
@@ -1,11 +1,19 @@
 var element = {} , cart = [];
-element.book = "book1";
-element.number = "book2";
-element.id = "book3";
-element.ISBN = "book4";
-element.CPF = "book5";
-element.CNPJ = "book6";
+
+for(i = 0 ; i < 5 ; i++){
+
+  element.book = "book1";
+  element.number = "book2";
+  element.id = "book3";
+  element.ISBN = "book4";
+  element.CPF = "book5";
+  element.CNPJ = "book6";
 
 cart.push(element);
+
+}
+
+
+
 
 console.log(cart);
