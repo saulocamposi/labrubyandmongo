078630b659e81d57c2af71dec2d871bf740c0dbf
saulocamposi <saulocamposi@gmail.com>
2016-04-17 17:05:28 -0300

    Add update lab

--- scriptadd.rb
+++ scriptadd.rb
@@ -3,42 +3,26 @@ require 'mongo'
 # Set your settings here ---> Start
 mongo_host = '127.0.0.1:27017'
 mongo_database = 'test'
-mongo_collection = 'mongo_base_processos'
+mongo_collection = 'book'
 sort_order = 'desc' # asc, desc are supported values
 # Set your settings here --> End
 
 Mongo::Logger.logger.level = ::Logger::FATAL
 db = Mongo::Client.new([mongo_host], :database => mongo_database)
 cols = []
+uniquekeys = []
 sort = 1
+
 if sort_order == 'desc'
   sort = -1
 end
-db[mongo_collection].find().limit(1).each do |document|
-  cols = document.keys
+
+db[mongo_collection].find().each do |document|
+  cols.push(document.keys)
 end
 
-cols.each do |col|
-  if col == '_id'
-    next
-  end
-
-  puts "Count of differnt values in column: #{col}"
-  puts "----------------------------------------------"
-  group1 = {
-    '$group': {
-      _id: "$#{col}",
-      count: { '$sum': 1 }
-    }
-  }
-  group2 = {
-    '$sort': {
-       count: sort   
-    }
-  }
-  agg = db[mongo_collection].aggregate([group1, group2])
-  agg.each do |doc|
-    puts "#{doc['_id']} | #{doc['count']}"
-  end
-  puts "\n\n\n\n"
+cols.each do |values|
+   uniquekeys.push(values).uniq
 end
+
+puts uniquekeys
