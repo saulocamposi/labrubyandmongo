9f901b239d6aabcb2fc580d7413c55722ff22adf
Saulo Camposi <saulocamposi@gmail.com>
2016-09-29 09:56:43 -0300

    Update boolean conditionals - differente conditionals for test string

--- booleanConditionals.rb
+++ booleanConditionals.rb
@@ -1,8 +1,22 @@
-puts "Get the command for the test"
-a = "true"
+a = "hello"
+puts "********************************************************"
+puts "Test Conditionals for String a in the ruby context"
+puts "********************************************************"
+puts ""
+puts "variable a inspect value: " + a.inspect
 
-if a?
-  puts "true"
-else
-  puts "false"
+if a == "hello"
+  puts "variable a is equal hello"
+end
+
+if a != nil
+  puts "it's not null"
+end
+
+unless a == nil
+  puts "it's not null as well"
+end
+
+if defined? a
+  puts "it's defined"
 end
