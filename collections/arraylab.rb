2871ab121ca1e77a41a9d03561b71c2bb6a8c8cf
Saulo Camposi <saulocamposi@gmail.com>
2016-10-02 11:35:52 -0300

    Add arraylab

---
+++ collections/arraylab.rb
@@ -0,0 +1,17 @@
+value = "banana"
+puts value
+puts "--------------"
+value = Array.new(3)
+puts value
+puts "--------------"
+value = ["banana","mango","orange"]
+puts value
+puts "--------------"
+value = Array.new(3, "name1")
+puts value
+puts "--------------"
+value = ["banana","mango","orange"]
+puts value[0]
+puts "--------------"
+value.pop
+puts value
